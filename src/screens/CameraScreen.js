8fdc7aacbfab59880f11034f5221190fb09650e4
palshah <palakshah28@gmail.com>
2018-10-20 13:40:07 -0400

    ES lint fixes

--- src/screens/CameraScreen.js
+++ src/screens/CameraScreen.js
@@ -15,42 +15,43 @@ export default class CameraExample extends React.Component {
 
   render() {
     const { hasCameraPermission } = this.state
+    const { type } = this.state
     if (hasCameraPermission === null) {
       return <View />
-    } else if (hasCameraPermission === false) {
+    }
+    if (hasCameraPermission === false) {
       return <Text>No access to camera</Text>
-    } else {
-      return (
-        <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
-            <View
+    }
+    return (
+      <View style={{ flex: 1 }}>
+        <Camera style={{ flex: 1 }} type={type}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'transparent',
+              flexDirection: 'row',
+            }}
+          >
+            <TouchableOpacity
               style={{
-                flex: 1,
-                backgroundColor: 'transparent',
-                flexDirection: 'row',
+                flex: 0.1,
+                alignSelf: 'flex-end',
+                alignItems: 'center',
+              }}
+              onPress={() => {
+                this.setState({
+                  type:
+                    { type } === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                })
               }}
             >
-              <TouchableOpacity
-                style={{
-                  flex: 0.1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center',
-                }}
-                onPress={() => {
-                  this.setState({
-                    type:
-                      this.state.type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back,
-                  })
-                }}
-              >
-                <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-              </TouchableOpacity>
-            </View>
-          </Camera>
-        </View>
-      )
-    }
+              <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
+    )
   }
 }
