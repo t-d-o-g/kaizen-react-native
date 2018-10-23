0f0a5d2707baae4d9728ba93f7e8be0471f6babc
Peter-Li1233 <minxiangli08@gmail.com>
2018-10-23 19:44:18 -0400

    add geolocation

--- mapIndex.js
+++ mapIndex.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import MapView from 'react-native-maps'
-import GeoCoder from 'react-native-geocoder'
+
 import EventModal from './src/components/EventModal'
 import EventCallout from './src/components/EventCallout'
 import EventInfo from './src/components/EventInfo'
-import { strikethrough } from 'ansi-colors'
 
 export default class Main extends React.Component {
   constructor(props) {
@@ -21,43 +20,56 @@ export default class Main extends React.Component {
       region: {
         latitude: 37.78825,
         longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
+        latitudeDelta: 0.00922,
+        longitudeDelta: 0.00421,
       },
       markerLocations: [
         {
           rotation: 78,
-          latitude: 37.78725,
-          longitude: -122.4318,
+          latitude: 40.731734,
+          longitude: -74.0605,
+          identifier: 'test1',
+          title: "it's noisy",
+          description: 'noisy after 10pm here',
+          status: 'open',
         },
         {
           rotation: -10,
-          latitude: 37.79015,
-          longitude: -122.4318,
+          latitude: 40.730255,
+          longitude: -74.0656,
+          identifier: 'test2',
+          title: "it's quiet",
+          description: 'too quiet in this community',
+          status: 'open',
         },
         {
           rotation: 262,
-          latitude: 37.78725,
-          longitude: -122.4348,
+          latitude: 40.732206,
+          longitude: -74.0669,
+          identifier: 'test3',
+          title: "it's rainy",
+          description: 'too much rain during fall here',
+          status: 'open',
         },
       ],
     }
     this._onPress = this._onPress.bind(this)
+    this._onMarkerPress = this._onMarkerPress.bind(this)
     this.initialRegion = {
-      latitude: 37.78825,
-      longitude: -122.4324,
+      latitude: 40.73136,
+      longitude: -74.0628,
       latitudeDelta: 0.00922,
       longitudeDelta: 0.00421,
     }
   }
 
   _onRegionChange(region) {
-    this.setState({ region })
+    // this.setState({ region: region });
     console.log(this.state.region)
   }
 
   _onPress(e) {
-    console.log(e.nativeEvent.coordinate)
+    // console.log(e.nativeEvent.coordinate);
     this.setState({
       markers: [
         //   ...this.state.markers,
@@ -70,30 +82,82 @@ export default class Main extends React.Component {
     console.log(this.state.markers)
   }
 
+  _onMarkerPress(e) {
+    console.log(e.nativeEvent)
+    var result = this.state.markerLocations.filter(obj => {
+      return obj.identifier === e.nativeEvent.id
+    })
+
+    console.log(result)
+    // this.setState({
+    //     region
+    // });
+    this.setState({
+      eventInfo: {
+        title: result[0].title,
+        description: result[0].description,
+        status: result[0].status,
+      },
+    })
+    this.setState({
+      eventModalVisible: true,
+    })
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log(position.coords)
+        let region = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }
+        this.setState({
+          region: region,
+        })
+      },
+      error => console.log(error),
+      { enableHighAccuracy: false, maximumAge: 1000 },
+    )
+  }
+
   render() {
     return (
       <View style={{ flex: 1 }}>
         <MapView
           showsUserLocation={true}
+          zoomEnabled={true}
           style={styles.fullScreenMap}
           initialRegion={this.initialRegion}
+          region={this.state.region}
           onRegionChange={this._onRegionChange.bind(this)}
           onPress={this._onPress}
         >
           {this.state.markerLocations.map((markerLocation, i) => (
-            <MapView.Marker key={i} coordinate={markerLocation}>
-              <MapView.Callout tooltip>
-                <EventCallout
-                  onClose={() => {
-                    console.log("what's up?")
-                  }}
-                  onReview={() => {
-                    console.log('linking to review page...')
-                  }}
-                >
-                  <EventInfo eventInfo={this.state.eventInfo} />
-                </EventCallout>
-              </MapView.Callout>
+            <MapView.Marker
+              key={i}
+              coordinate={markerLocation}
+              onPress={this._onMarkerPress}
+              identifier={markerLocation.identifier}
+            >
+              {/* <MapView.Callout 
+                    tooltip
+                    >
+                        <EventCallout
+                          onClose={() => {
+                              console.log("what's up?");
+                          }}
+                          onReview={() => {
+                            console.log("linking to review page...");
+                          }}
+                        >
+                            <EventInfo
+                              eventInfo={this.state.eventInfo}
+                            />
+                        </EventCallout>
+                    </MapView.Callout> */}
             </MapView.Marker>
           ))}
           {this.state.markers.map((marker, i) => (
