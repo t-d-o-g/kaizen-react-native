82e351c0b11ffc89acbe10cc26b379d37ce13aa4
vsaraogi <mailvsaraogi@gmail.com>
2018-10-29 00:31:03 -0400

    Login support on the front-end

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -14,11 +14,52 @@ import {
   Left,
   Right,
 } from 'native-base'
+import API from '../../utils/API'
+import userInfo from '../../utils/userInfo'
 
 export default class LoginScreen extends React.Component {
   static navigationOptions = {
     title: 'Login',
   }
+
+  state = {
+    username: undefined,
+    password: undefined,
+  }
+
+  loginUser = () => {
+    // console.warn("In login user")
+    // VIK_TODO: Do validation
+    // VIK_TODO: Make changes so we use email instead of username
+    API.loginUser({ email: this.state.username, password: this.state.password })
+      .then(response => {
+        console.warn(JSON.stringify(response))
+      })
+      .catch(error => {
+        console.warn(error)
+      })
+    // console.warn("submitted");
+  }
+
+  // saveAsyncStorage = () => {
+  //   userInfo.saveUserInfo('This is test user save')
+  //   .then(response => {
+  //     console.warn( JSON.stringify(response))
+  //   })
+  //   .catch(error => {
+  //     console.warn( JSON.stringify(error))
+  //   })
+  // }
+
+  // retrieveAsyncStorage = () => {
+  //   userInfo.getUserInfo()
+  //   .then(response => {
+  //     console.warn( JSON.stringify(response))
+  //   })
+  //   .catch(error => {
+  //     console.warn( JSON.stringify(error))
+  //   })
+  // }
 
   render() {
     const { navigation } = this.props
@@ -36,16 +77,32 @@ export default class LoginScreen extends React.Component {
           <Form>
             <Item floatingLabel>
               <Label> Username </Label>
-              <Input style={styles.input} />
+              <Input
+                style={styles.input}
+                value={this.state.username}
+                onChangeText={inputValue => this.setState({ username: inputValue })}
+              />
             </Item>
             <Item floatingLabel last>
               <Label> Password </Label>
-              <Input style={styles.input} />
+              <Input
+                style={styles.input}
+                secureTextEntry={true}
+                value={this.state.password}
+                onChangeText={inputValue => this.setState({ password: inputValue })}
+              />
             </Item>
-            <Button primary style={styles.loginButton}>
-              <Text style={styles.loginText}> Login </Text>
-            </Button>
           </Form>
+          <Button primary style={styles.loginButton} onPress={this.loginUser}>
+            <Text style={styles.loginText}> Login </Text>
+          </Button>
+          {/* VIK_DEBUG: Remove following later on */}
+          {/* <Button primary style={styles.loginButton} onPress={this.saveAsyncStorage}>
+              <Text style={styles.loginText}> Save in AsyncStorage </Text>
+            </Button>
+            <Button primary style={styles.loginButton} onPress={this.retrieveAsyncStorage}>
+              <Text style={styles.loginText}> Retrieve from AsyncStorage </Text>
+            </Button> */}
           <Text style={styles.registerText}>
             {' '}
             Do not have an account?
